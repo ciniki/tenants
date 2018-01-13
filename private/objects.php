6775890887f78e4778f16730958d38aaf01bffb7
Andrew Rivett <veggiefrog@gmail.com>
2018-01-13 18:55:39 -0500

    Removed reports into the reporting module

--- private/objects.php
+++ private/objects.php
@@ -29,48 +29,6 @@ function ciniki_tenants_objects($ciniki) {
             ),
         'history_table' => 'ciniki_tenant_history',
         );
-    $objects['report'] = array(
-        'name'=>'Reports',
-        'sync'=>'yes',
-        'table'=>'ciniki_tenant_reports',
-        'o_name'=>'report',
-        'o_container'=>'reports',
-        'fields'=>array(
-            'title'=>array('name'=>'Title'),
-            'frequency'=>array('name'=>'Frequency'),
-            'flags'=>array('name'=>'Options', 'default'=>0x03),
-            'next_date'=>array('name'=>'Next Date'),
-            ),
-        'history_table'=>'ciniki_tenant_history',
-        );
-    $objects['reportuser'] = array(
-        'name'=>'Report Users',
-        'sync'=>'yes',
-        'table'=>'ciniki_tenant_report_users',
-        'o_name'=>'reportuser',
-        'o_container'=>'reportusers',
-        'fields'=>array(
-            'report_id'=>array('name'=>'Report', 'ref'=>'ciniki.tenants.report'),
-            'user_id'=>array('name'=>'User', 'ref'=>'ciniki.users.user'),
-            ),
-        'history_table'=>'ciniki_tenant_history',
-        );
-    $objects['reportblock'] = array(
-        'name'=>'Report Blocks',
-        'sync'=>'yes',
-        'table'=>'ciniki_tenant_report_blocks',
-        'o_name'=>'reportblock',
-        'o_container'=>'reportblock',
-        'fields'=>array(
-            'report_id'=>array('name'=>'Report', 'ref'=>'ciniki.tenants.report'),
-            'btype'=>array('name'=>'Block Type'),
-            'title'=>array('name'=>'Title', 'default'=>''),
-            'sequence'=>array('name'=>'Order'),
-            'block_ref'=>array('name'=>'Block'),
-            'options'=>array('name'=>'Options', 'default'=>''),
-            ),
-        'history_table'=>'ciniki_tenant_history',
-        );
     
     return array('stat'=>'ok', 'objects'=>$objects);
 }
