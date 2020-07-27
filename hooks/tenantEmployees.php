847c261e7cd96262708dd5282fc17a24a424789f
Andrew Rivett <veggiefrog@gmail.com>
2020-07-27 16:08:12 -0400

    Added hook for getting list of employees

---
+++ hooks/tenantEmployees.php
@@ -0,0 +1,48 @@
+<?php
+//
+// Description
+// -----------
+// Return the list of tenant owners.
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_tenants_hooks_tenantEmployees(&$ciniki, $tnid, $args) {
+
+    //
+    // Select the owners only
+    //
+    $strsql = "SELECT ciniki_tenant_users.user_id, "
+        . "ciniki_tenant_users.status, "
+        . "ciniki_users.email, "
+        . "ciniki_users.username, "
+        . "ciniki_users.firstname, "
+        . "ciniki_users.lastname, "
+        . "ciniki_users.display_name "
+        . "FROM ciniki_tenant_users, ciniki_users "
+        . "WHERE ciniki_tenant_users.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+        . "AND ciniki_tenant_users.package = 'ciniki' "
+        . "AND ciniki_tenant_users.permission_group IN ('owners', 'employees') "
+        . "AND ciniki_tenant_users.status = 10 "      // Active employees only
+        . "AND ciniki_tenant_users.user_id = ciniki_users.id "
+        . "AND ciniki_users.status < 11 "
+        . "ORDER BY ciniki_users.display_name "
+        . "";
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+    $rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.tenants', array(
+        array('container'=>'users', 'fname'=>'user_id',
+            'fields'=>array('user_id', 'firstname', 'lastname', 'display_name', 'email')),
+        ));
+    if( $rc['stat'] != 'ok' ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.tenants.2', 'msg'=>'Unable to get list of owners', 'err'=>$rc['err']));
+    }
+    if( !isset($rc['users']) ) {
+        return array('stat'=>'ok', 'users'=>array());
+    }
+
+    return array('stat'=>'ok', 'users'=>$rc['users']);
+}
+?>
