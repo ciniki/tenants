f9275188d432ab5942ff00f0a192835ee657075f
Andrew Rivett <veggiefrog@gmail.com>
2021-01-29 20:59:28 -0500

    Updated scripts

--- scripts/checkdomains.php
+++ scripts/checkdomains.php
@@ -16,7 +16,6 @@ if( !file_exists($ciniki_root . '/ciniki-api.ini') ) {
 // loadMethod is required by all function to ensure the functions are dynamically loaded
 require_once($ciniki_root . '/ciniki-mods/core/private/loadMethod.php');
 require_once($ciniki_root . '/ciniki-mods/core/private/init.php');
-require_once($ciniki_root . '/ciniki-mods/core/private/checkModuleFlags.php');
 require_once($ciniki_root . '/ciniki-mods/core/private/dbHashQuery.php');
 require_once($ciniki_root . '/ciniki-mods/core/private/dbQuote.php');
 require_once($ciniki_root . '/ciniki-mods/core/private/objectUpdate.php');
